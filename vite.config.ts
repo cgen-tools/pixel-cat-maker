afae8c2bd92237a0a1150e0a8146701b0346f79f
Lara <63805048+larkgz@users.noreply.github.com>
2025-08-23 22:19:52 -0400

    chore: skeleton for predict-offspring

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,19 @@
 import { defineConfig } from "vite";
+import { dirname, resolve } from 'node:path'
+import { fileURLToPath } from 'node:url'
+
+const __dirname = dirname(fileURLToPath(import.meta.url))
 
 // https://vite.dev/config/
 export default defineConfig({
   base: "./",
   build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, "index.html"),
+        "predict-offspring": resolve(__dirname, "predict-offspring.html"),
+      }
+    },
     sourcemap: true,
   },
 });
