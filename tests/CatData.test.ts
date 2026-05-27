672c981edf60a31456a560aeba0b960372b52fbe
Lara <63805048+larkgz@users.noreply.github.com>
2026-05-26 23:08:36 -0400

    test: update test

--- tests/CatData.test.ts
+++ tests/CatData.test.ts
@@ -1,13 +1,13 @@
 import { expect, test } from 'vitest';
 import CatData from '../src/library/CatData';
 
-const catURL = "https://cgen-tools.github.io/pixel-cat-maker/?shading=true&reverse=true&isTortie=false&backgroundColour=rgb(0+0+0+%2F+0)&tortieMask=ONE&tortieColour=BLACK&tortiePattern=Classic&peltName=Speckled&spriteNumber=6&colour=BLACK&tint=pink&skinColour=PEACH&eyeColour=YELLOW&eyeColour2=&whitePatches=&points=&whitePatchesTint=&vitiligo=&accessory=HOLLY&scar=&version=v1";
+const catURL = "https://cgen-tools.github.io/pixel-cat-maker/?shading=true&reverse=true&isTortie=false&backgroundColour=rgb(0+0+0+%2F+0)&tortieMask=ONE&tortieColour=BLACK&tortiePattern=Classic&peltName=Speckled&spriteNumber=6&colour=BLACK&tint=pink&skinColour=PEACH&eyeColour=YELLOW&eyeColour2=&whitePatches=&points=&whitePatchesTint=&vitiligo=&accessory=CRIMSON&scar=&version=v1";
 
-test('test cat URL', () => {
+test('test converted cat URL', () => {
   expect(CatData.
     fromURL(catURL))
     .toMatchObject({
-      accessory: "HOLLY",
+      accessory: "LEATHER_crimson",
       backgroundColour: "rgb(0 0 0 / 0)",
       colour: "BLACK",
       eyeColour: "YELLOW",
@@ -19,7 +19,7 @@ test('test cat URL', () => {
       scar: null,
       shading: true,
       skinColour: "PEACH",
-      spriteNumber: 6,
+      spriteNumber: 12,
       tint: "pink",
       tortieColour: "BLACK",
       tortieMask: "ONE",
